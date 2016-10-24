6801d08f4b0a90180226d462bbc7ba68cb9795a4
Natalia Hess <nhess@natalia-hesss-macbook-pro.local>
2016-10-24 15:19:19 -0700

    create seed data. FIX ME css issue

--- ProjectVagabond/app/controllers/cities_controller.rb
+++ ProjectVagabond/app/controllers/cities_controller.rb
@@ -14,5 +14,6 @@ class CitiesController < ApplicationController
   private
 
   def city_params
+    params.require(:city).permit(:name, :country, :picture)
   end
 end
--- ProjectVagabond/db/seeds.rb
+++ ProjectVagabond/db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+cities = City.create([
+  {
+    name: "The Shire",
+    country: "Eriador",
+    picture: "https://s-media-cache-ak0.pinimg.com/originals/28/59/1d/28591dcb69a0d6ad3701dab8939d3561.jpg"
+  },
+  {
+    name: "Rivendell",
+    country: "Eriador",
+    picture: "http://vignette4.wikia.nocookie.net/lotr/images/5/53/Rivendell_-_The_Hobbit.PNG/revision/latest?cb=20121111111511"
+  },
+  {
+    name: "Isengard",
+    country: "Rohan",
+    picture: "http://vignette3.wikia.nocookie.net/lotr/images/4/42/Destruction_of_Isengard_-_Day_After.jpg/revision/latest?cb=20120606113627"
+  },
+  {
+    name: "Helm's Deep",
+    country: "Rohan",
+    picture: "http://www.lordoftherings.net/legend/lands/helmsdeep/images/helmsdeep1_large.jpg"
+  },
+  {
+    name: "Mount Doom",
+    country: "Mordor",
+    picture: "http://www.thelandofshadow.com/wp-content/uploads/2016/02/Orodruin6.jpg"
+  },
+  {
+    name: "Shelob's Lair",
+    country: "Mordor",
+    picture: "http://blindfolly.org/blindfolly/wp-content/uploads/2015/08/lordoftherings_-spiderweb600.jpg"
+  }])
