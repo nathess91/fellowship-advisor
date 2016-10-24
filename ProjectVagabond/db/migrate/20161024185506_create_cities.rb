a630bbdfe7852fa2a65bfd8deef353ec3f131771
Natalia Hess <nhess@natalia-hesss-macbook-pro.local>
2016-10-24 11:59:51 -0700

    generate models, migrations, and schema

---
+++ ProjectVagabond/db/migrate/20161024185506_create_cities.rb
@@ -0,0 +1,11 @@
+class CreateCities < ActiveRecord::Migration[5.0]
+  def change
+    create_table :cities do |t|
+      t.string :name
+      t.string :country
+      t.string :picture
+
+      t.timestamps
+    end
+  end
+end
