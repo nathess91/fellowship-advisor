9c15e63b01c51a1f9d21635bd38d54debc120d39
Natalia Hess <nhess@natalia-hesss-macbook-pro.local>
2016-10-24 12:17:03 -0700

    set up all routes and begin city views/controller actions

--- ProjectVagabond/app/controllers/cities_controller.rb
+++ ProjectVagabond/app/controllers/cities_controller.rb
@@ -1,2 +1,18 @@
 class CitiesController < ApplicationController
+  def index
+    @cities = City.all
+  end
+
+  def new
+    @city = City.new
+  end
+
+  def create
+    @city = City.create(city_params)
+  end
+
+  private
+
+  def city_params
+  end
 end
--- ProjectVagabond/config/routes.rb
+++ ProjectVagabond/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   root to: 'cities#index'
+
+
+  get '/cities', to: 'cities#index', as: 'cities'
+  get '/cities/:name', to: 'cities#show', as: 'city'
+
+  resources :users, except: :index
+  resources :posts
 end
