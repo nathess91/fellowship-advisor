08f6399096da1a06eedda380e1980e50182576c7
Natalia Hess <nhess@natalia-hesss-macbook-pro.local>
2016-10-26 11:37:05 -0700

    setup comment model and controller

--- ProjectVagabond/app/models/post.rb
+++ ProjectVagabond/app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   has_one :user, through: :post_users
   belongs_to :city
+  has_many :comments
 
   validates :title , length: { in:1..200, too_long: "%{count} characters is the maximum allowed" }
   validates :content, :title, presence: true
