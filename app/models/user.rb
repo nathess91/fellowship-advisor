860d2ecf99b6c061c9cad5112c2cdc72869fe0a2
Natalia Hess <nhess@natalia-hesss-macbook-pro.local>
2016-10-26 17:27:30 -0700

    Toby found bugs...fixed them

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
   has_many :comments
 
   validates :email, uniqueness: true
+  validates :name, :email, :current_city, :password, presence: true
 
   def self.confirm(params)
     @user = User.find_by({email: params[:email]})
